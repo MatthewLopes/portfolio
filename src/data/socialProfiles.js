8aeed6aac6d3de9c5723c011b669d583f664bd58
unknown <mattlopes1997@gmail.com>
2020-02-05 19:19:59 -0500

    Adding social section with links to social platforms

---
+++ src/data/socialProfiles.js
@@ -0,0 +1,23 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+
+const SOCIAL_PROFILES = [
+    {
+        id: 1,
+        link: 'mailto:mattlopes1997@gmail.com',
+        image: emailIcon
+    },
+    {
+        id: 2,
+        link: 'https://github.com/MatthewLopes',
+        image: githubIcon
+    },
+    {
+        id: 3,
+        link: 'https://www.linkedin.com/in/matthew-lopes-69163812a/',
+        image: linkedinIcon
+    }
+];
+
+export default SOCIAL_PROFILES;
